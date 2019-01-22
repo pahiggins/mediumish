bae14ede70723ca2fa9c93b73a01741a0a87f35f
Peter A. Higgins <pahiggins@cenabo.com>
2019-01-22 10:02:13 +0000

    Add comments heading

--- src/blocks/Comments/index.jsx
+++ src/blocks/Comments/index.jsx
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { SpinLoader } from 'react-css-loaders';
+import styled from 'styled-components';
 import Comment from '../Comment';
 import Section from '../../elements/Section';
 import * as api from '../../utils';
+
+const H3 = styled.h3`
+  margin-bottom: 2rem;
+  font-size: 1.4rem;
+  font-weight: 600;
+  color: rgba(0, 0, 0, 0.68);
+`;
 
 class Comments extends Component {
   state = {
@@ -19,9 +27,12 @@ class Comments extends Component {
         {loading ? (
           <SpinLoader size={5} color="#ccc" />
         ) : (
-          comments.map(comment => (
-            <Comment key={comment.comment_id} comment={comment} />
-          ))
+          <Fragment>
+            <H3>Comments</H3>
+            {comments.map(comment => (
+              <Comment key={comment.comment_id} comment={comment} />
+            ))}
+          </Fragment>
         )}
       </Section>
     );
