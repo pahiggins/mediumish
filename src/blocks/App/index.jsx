d5cfbbd33faf98e48dd5e2b5f8c8cc0020f6beac
Peter A. Higgins <pahiggins@cenabo.com>
2019-01-23 07:45:59 +0000

    Add styling to ArticleAdd component

--- src/blocks/App/index.jsx
+++ src/blocks/App/index.jsx
@@ -5,6 +5,7 @@ import Header from '../../blocks/Header';
 import MainNav from '../../blocks/MainNav';
 import Main from '../../elements/Main';
 import Articles from '../Articles';
+import ArticleAdd from '../ArticleAdd';
 import ArticleDetails from '../ArticleDetails';
 
 class App extends Component {
@@ -18,7 +19,7 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={Articles} />
               {/* <Route exact path="/log-in" component={LogIn} /> */}
-              {/* <Route exact path="/new-article" component={ArticleAdd} /> */}
+              <Route exact path="/new-article" component={ArticleAdd} />
               <Route exact path="/topic/:slug" component={Articles} />
               <Route path="/:username/:articleId" component={ArticleDetails} />
             </Switch>
---
+++ src/blocks/ArticleAdd/index.jsx
@@ -0,0 +1,161 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import styled from 'styled-components';
+import Section from '../../elements/Section';
+import * as api from '../../utils';
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  font-family: 'Libre Baskerville', serif;
+  margin-bottom: 2rem;
+  height: 5.4rem;
+  font-size: 4.2rem;
+  border: none;
+  color: rgba(0, 0, 0, 0.84);
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.24);
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const TextArea = styled.textarea`
+  font-family: 'Libre Baskerville', serif;
+  margin-bottom: 2rem;
+  height: 55vh;
+  font-size: 2rem;
+  border: none;
+  color: rgba(0, 0, 0, 0.84);
+  resize: none;
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.24);
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  padding: 0 1.5rem;
+  margin-right: ${props => props.marginRight || 0};
+  height: 3.7rem;
+  line-height: 3.7rem;
+  background-color: ${props => props.backgroundColor};
+  border-color: ${props => props.borderColor};
+  border-radius: 0.4rem;
+  font-size: 1.6rem;
+  text-align: center;
+  color: ${props => props.color};
+  cursor: pointer;
+  transition: borderColor 0.5s;
+
+  &:hover {
+    background-color: ${props => props.backgroundColorHover};
+    border-color: ${props => props.borderColorHover};
+    color: ${props => props.colorHover};
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:select {
+    background-color: ${props => props.backgroundColorSelect};
+    border-color: ${props => props.borderColorSelect};
+  }
+`;
+
+class ArticleAdd extends Component {
+  state = {
+    title: '',
+    body: '',
+    redirect: '',
+  };
+
+  render() {
+    const { title, body, redirect } = this.state;
+
+    return (
+      <Section inputWidth="80%" inputMargin="0 auto">
+        <Form onSubmit={this.handleSubmit}>
+          <Input
+            type="text"
+            id="title"
+            value={title}
+            onChange={this.handleChange}
+            placeholder="Title"
+            autoFocus
+          />
+          <TextArea
+            type="text"
+            id="body"
+            value={body}
+            onChange={this.handleChange}
+            placeholder="Tell your story..."
+            rows="5"
+            cols="33"
+          />
+          <Buttons>
+            <Button
+              type="submit"
+              borderColor={'rgba(0, 0, 0, 0.24)'}
+              borderColorHover={'rgba(0, 0, 0, 0.54)'}
+              color={'rgba(0, 0, 0, 0.54)'}
+              marginRight={'1.5rem'}
+              onClick={() => {}}
+            >
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              backgroundColorHover={'rgba(3, 168, 124, 1)'}
+              borderColor={'rgba(3, 168, 124, 1)'}
+              color={'rgba(3, 168, 124, 1)'}
+              colorHover={'#fff'}
+              backgroundColorSelect={'rgba(3, 168, 124, 0.8)'}
+              borderColorSelect={'rgba(3, 168, 124, 0.8)'}
+              onClick={() => {}}
+            >
+              Publish
+            </Button>
+          </Buttons>
+        </Form>
+      </Section>
+    );
+  }
+
+  handleChange = event => {
+    const { value, id } = event.target;
+    this.setState({
+      [id]: value,
+    });
+  };
+
+  handleSubmit = event => {
+    const { title, body } = this.state;
+
+    event.preventDefault();
+
+    this.props.addArticle({ title, body });
+    this.setState({
+      title: '',
+      body: '',
+    });
+  };
+}
+
+export default ArticleAdd;
