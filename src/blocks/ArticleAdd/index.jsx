848fc39ab02fd6831d041017f8c76ec8e2db16b9
Peter A. Higgins <pahiggins@cenabo.com>
2019-01-24 14:37:35 +0000

    Limit adding articles to signed in users

--- src/blocks/ArticleAdd/index.jsx
+++ src/blocks/ArticleAdd/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { SpinLoader } from 'react-css-loaders';
 import styled from 'styled-components';
+import AuthContext from '../App/AuthContext';
 import Section from '../../elements/Section';
 import Button from '../../elements/Button';
 import * as api from '../../utils';
@@ -86,65 +87,71 @@ class ArticleAdd extends Component {
     const { topics, title, topic, body, loading } = this.state;
 
     return (
-      <Section inputWidth="80%" inputMargin="0 auto">
-        {loading ? (
-          <SpinLoader size={5} color="#ccc" />
-        ) : (
-          <Form onSubmit={this.handleSubmit}>
-            <Input
-              type="text"
-              id="title"
-              value={title}
-              onChange={this.handleChange}
-              placeholder="Title"
-              autoFocus
-            />
-            <Select
-              type="select"
-              id="topic"
-              value={topic}
-              name="select"
-              onChange={this.handleChange}
-            >
-              {topics.map(({ slug }) => (
-                <option key={slug}>{capitalizeFirstLetter(slug)}</option>
-              ))}
-            </Select>
-            <TextArea
-              type="text"
-              id="body"
-              value={body}
-              onChange={this.handleChange}
-              placeholder="Tell your story..."
-              rows="5"
-              cols="33"
-            />
-            <Buttons>
-              <Button
-                type="submit"
-                borderColor={'rgba(0, 0, 0, 0.24)'}
-                borderColorHover={'rgba(0, 0, 0, 0.54)'}
-                color={'rgba(0, 0, 0, 0.54)'}
-                marginRight={'1.5rem'}
-                onClick={this.handleClick}
-              >
-                Cancel
-              </Button>
-              <Button
-                type="submit"
-                backgroundColorHover={'rgba(3, 168, 124, 1)'}
-                borderColor={'rgba(3, 168, 124, 1)'}
-                color={'rgba(3, 168, 124, 1)'}
-                colorHover={'#fff'}
-                backgroundColorSelect={'rgba(3, 168, 124, 0.8)'}
-                borderColorSelect={'rgba(3, 168, 124, 0.8)'}
-              >
-                Publish
-              </Button>
-            </Buttons>
-          </Form>
-        )}
-      </Section>
+      <AuthContext.Consumer>
+        {({ username }) =>
+          username && (
+            <Section inputWidth="80%" inputMargin="0 auto">
+              {loading ? (
+                <SpinLoader size={5} color="#ccc" />
+              ) : (
+                <Form onSubmit={this.handleSubmit}>
+                  <Input
+                    type="text"
+                    id="title"
+                    value={title}
+                    onChange={this.handleChange}
+                    placeholder="Title"
+                    autoFocus
+                  />
+                  <Select
+                    type="select"
+                    id="topic"
+                    value={topic}
+                    name="select"
+                    onChange={this.handleChange}
+                  >
+                    {topics.map(({ slug }) => (
+                      <option key={slug}>{capitalizeFirstLetter(slug)}</option>
+                    ))}
+                  </Select>
+                  <TextArea
+                    type="text"
+                    id="body"
+                    value={body}
+                    onChange={this.handleChange}
+                    placeholder="Tell your story..."
+                    rows="5"
+                    cols="33"
+                  />
+                  <Buttons>
+                    <Button
+                      type="button"
+                      borderColor={'rgba(0, 0, 0, 0.24)'}
+                      borderColorHover={'rgba(0, 0, 0, 0.54)'}
+                      color={'rgba(0, 0, 0, 0.54)'}
+                      marginRight={'1.5rem'}
+                      onClick={this.handleClick}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      type="button"
+                      backgroundColorHover={'rgba(3, 168, 124, 1)'}
+                      borderColor={'rgba(3, 168, 124, 1)'}
+                      color={'rgba(3, 168, 124, 1)'}
+                      colorHover={'#fff'}
+                      backgroundColorSelect={'rgba(3, 168, 124, 0.8)'}
+                      borderColorSelect={'rgba(3, 168, 124, 0.8)'}
+                    >
+                      Publish
+                    </Button>
+                  </Buttons>
+                </Form>
+              )}
+            </Section>
+          )
+        }
+      </AuthContext.Consumer>
     );
   }
 
